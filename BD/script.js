55ba3168d97af1187c5df30bca800b7b04e72950
OhMyTlswp <54995327+OhMyTlswp@users.noreply.github.com>
2020-01-22 17:15:12 +0300

    add

--- BD/script.js
+++ BD/script.js
@@ -1,15 +1,24 @@
 var imgBox = document.getElementById('img-box'),
-  main = document.querySelector('.main');
+  main = document.querySelector('.main'),
+  body = document.querySelector('body'),
+  sliderElement = document.querySelectorAll('.slider');
+sliderElement[1].style.transform = 'translateX(' + -1600 + 'px)';
 imgBox.style
+window.pageXOffset = 0;
 imgBoxHeight = imgBox.offsetHeight
 window.addEventListener('scroll', function() {
   if (window.pageYOffset > 50) {
     if (window.pageYOffset < 250) {
+      window.pageXOffset = 0;
       imgBox.style.transform = 'translateY(' + window.pageYOffset + 'px) scale(' + 50 / window.pageYOffset + ')';
     }
     //imgBox.style.transform = 'scale(' + 50 / window.pageYOffset + ')';
   }
   if (window.pageYOffset == 0) {
     imgBox.style.transform = '';
+    body.style.overflowX = 'hidden';
   }
+  sliderElement[0].style.transform = 'translateX(' + -window.pageYOffset / 5 + 'px)';
+  sliderElement[1].style.transform = 'translateX(' + (-1600 + window.pageYOffset / 5) + 'px)';
+  sliderElement[2].style.transform = 'translateX(' + -window.pageYOffset / 5 + 'px)';
 });
